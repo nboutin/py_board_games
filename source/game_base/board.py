60d53a1a6bb09ec4c1450e4278ed65f54253e48e
nboutin <nicolas.boutin.work@gmail.com>
2020-01-08 18:05:09 +0100

    wip: Add game_base board

---
+++ source/game_base/board.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Point():
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    def __str__(self):
+        return "({},{})".format(self._x, self._y)
+
+
+class Board():
+
+    def __init__(self, column_count, row_count):
+        '''ToDo: try with Numpy array for better performance ?'''
+
+        self._column = column_count  # X
+        self._row = row_count        # Y
+
+        self._grid = [[None for x in range(self._column)]
+                      for y in range(self._row)]
+
+        self._cell_free_count = self._column * self._row
+
+    def has_free_cell(self):
+        return self._cell_free_count > 0
+
+    @property
+    def cell_used_count(self):
+        return self._column * self._row - self._cell_free_count
+
+    @property
+    def grid(self):
+        return self._grid
+
+    def play(self, point, token):
+        ''' grid coordinate are reversed (y,x)'''
+        x, y = point.x, point.y
+
+        if 0 > x or x >= self._column or 0 > y or y >= self._row:
+            return False
+
+        # Check free cell
+        if self._grid[y][x] is not None:
+            return False
+
+        # Add token to grid
+        self._grid[y][x] = token
+        self._cell_free_count -= 1
+
+        return True
+
+    def undo(self, point):
+        x, y = point.x, point.y
+
+        if 0 > x or x >= self._column or 0 > y or y >= self._row:
+            return False
+
+        self._grid[y][x] = None
+        self._cell_free_count += 1
+
+        return True
+    
+    def check_line_horizontal(self, x_start, x_end, y, line_test):
+        row = self._grid[y]
+        return self._check_line(row, x_start, x_end, line_test)
+
+    def check_line_vertical(self, y_start, y_end, x, line_test):
+        column = [self._grid[y][x] for y in range(self._row)]
+        return self._check_line(column, y_start, y_end, line_test)
+
+    def _check_line(self, line, start, end, pattern):
+        '''
+        check if line_test is present in row y between start and end
+        @todo explain bound check (built-in list ?)
+        '''
+        length = len(pattern)
+        for x in range(start, end):
+            if line[x:x + length] == pattern:
+                return True
+        return False
+
+    def __str__(self):
+        ''' string representation for debug purpose'''
+        s = '\n'
+        for row in self._grid:
+            for cell in row:
+                s += "{}|".format(cell)
+            s += '\n'
+        return s
---
+++ test/game_base/test_board.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+import sys
+import os
+sys.path.insert(0, os.path.join(sys.path[0], 'source'))
+
+from game_base.board import (Board, Point)
+
+
+class TestBoard(unittest.TestCase):
+
+    def test_init_1_1(self):
+        '''
+        Test construction parameter and consistency with properties
+        '''
+        board = Board(1,1)
+        self.assertEqual(board._cell_free_count, 1)
+        self.assertTrue(board.has_free_cell)
+        self.assertEqual(board.cell_used_count, 0)
+
+#     def test_has_free_cell(self):
+#         board = Board()
+#
+#         for x in range(3):
+#             for y in range(3):
+#                 self.assertTrue(board.has_free_cell())
+#                 self.assertTrue(board.play(Point(x, y), Token.CROSS))
+#
+#         self.assertFalse(board.has_free_cell())
+#
+#     def test_played_cell_count(self):
+#         board = Board()
+#         self.assertEqual(board.played_cell_count, 0)
+#         self.assertTrue(board.play(Point(0, 0), Token.CROSS))
+#         self.assertEqual(board.played_cell_count, 1)
+#
+#     def test_play_busy_cell(self):
+#         board = Board()
+#         self.assertTrue(board.play(Point(0, 0), Token.CROSS))
+#         self.assertFalse(board.play(Point(0, 0), Token.CROSS))
+#
+#     def test_boundaries(self):
+#
+#         board = Board()
+#         self.assertFalse(board.play(Point(-1, 0), Token.CROSS))
+#         self.assertFalse(board.play(Point(-2, -3), Token.CROSS))
+#         self.assertFalse(board.play(Point(0, -4), Token.CROSS))
+#         self.assertFalse(board.play(Point(3, 0), Token.CROSS))
+#         self.assertFalse(board.play(Point(3, 5), Token.CROSS))
+#         self.assertFalse(board.play(Point(0, 5), Token.CROSS))
+
+
+if __name__ == '__main__':
+    unittest.main()
